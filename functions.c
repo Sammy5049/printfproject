5f6ba14dca05dd6b2b849716464adbe741696f93
Damilola Adeola <samadedammy@gmail.com>
2023-07-24 16:08:28 -0700

    Print binary numbers

--- functions.c
+++ functions.c
@@ -139,3 +139,49 @@ int print_int(va_list types, char buffer[],
 
 	return (write_number(is_negative, k, buffer, flags, width, precision, size));
 }
+
+/************************* PRINT BINARY NUMBER2 *************************/
+/**
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specify
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int k, j, i, sum;
+	unsigned int a[32];
+	int counter;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	k = va_arg(types, unsigned int);
+	j = 2147483648; /* (2 ^ 31) */
+	a[0] = k / j;
+	for (i = 1; i < 32; i++)
+	{
+		j /= 2;
+		a[i] = (k / j) % 2;
+	}
+	for (i = 0, sum = 0, counter = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			counter++;
+		}
+	}
+	return (counter);
+}
