0e3c268cb95a153c24bae402edbcfacf888524a6
mayor167 <adenijimayokun@gmail.com>
2023-07-24 15:47:41 -0700

    Print integer and decimal

--- functions.c
+++ functions.c
@@ -96,3 +96,46 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(size);
 	return (write(1, "%%", 1));
 }
+/********* PRINT INT NUMBER1 *********/
+/**
+ * print_int - Print int
+ * @types: Lists of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specify
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_int(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	int k = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int numb;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buffer[k--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	numb = (unsigned long int)n;
+
+	if (n < 0)
+	{
+		numb = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
+
+	while (numb > 0)
+	{
+		buffer[k--] = (numb % 10) + '0';
+		numb /= 10;
+	}
+
+	k++;
+
+	return (write_number(is_negative, k, buffer, flags, width, precision, size));
+}
